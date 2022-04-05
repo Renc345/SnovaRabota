8aa9bf64adbef5493903f4da36c34bd1a48880d3
ALEXDEX8084 <98825004+ALEXDEX8084@users.noreply.github.com>
2022-04-05 10:13:05 +0300

    Add files via upload

---
+++ MelegaPr9mdk/Pages/PageDoWhile.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelegaPr9mdk.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageDoWhile.xaml
+    /// </summary>
+    public partial class PageDoWhile : Page
+    {
+        public PageDoWhile()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext1_Click(object sender, RoutedEventArgs e)
+        {
+            ClsFrame.frmObject.Navigate(new PageFor());
+        }
+
+        private void btnCalculate1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnback_Click(object sender, RoutedEventArgs e)
+        {
+            ClsFrame.frmObject.Navigate(new PageWhile());
+        }
+    }
+}
---
+++ MelegaPr9mdk/Pages/PageFor.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MelegaPr9mdk.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageFor.xaml
+    /// </summary>
+    public partial class PageFor : Page
+    {
+        public PageFor()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalculate2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnback1_Click(object sender, RoutedEventArgs e)
+        {
+            ClsFrame.frmObject.Navigate(new PageDoWhile());
+        }
+    }
+}
